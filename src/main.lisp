5821d4c230be2abf4d491f8038287e72a4043a89
myTerminal <team.terminal@gmail.com>
2023-06-14 11:26:18 -0500

    include all available devices in commands

--- src/main.lisp
+++ src/main.lisp
@@ -3,26 +3,11 @@
 
 (in-package :main)
 
-(defun connect-external-displays (primary-device external-devices position)
+(defun connect-external-displays (primary-device external-devices disconnected-devices position)
   "Connects all supplied connected displays."
   (if (not (member position '("left" "right") :test #'string-equal))
       (princ "Please specify \"left/right\"!")
-      (execute-in-system (concatenate 'string
-                                      "xrandr --output "
-                                      "\"" primary-device "\""
-                                      " --auto "
-                                      (apply #'concatenate 'string
-                                             (mapcar (lambda (d)
-                                                       (concatenate 'string
-                                                                    "--output "
-                                                                    "\"" d "\""
-                                                                    " --auto " (get-orientation position) " "
-                                                                    "\"" primary-device "\" "))
-                                                     external-devices))))))
-
-(defun disconnect-external-displays (primary-device external-devices)
-  "Connects all supplied connected displays."
-  (execute-in-system (concatenate 'string
+      (log-to-stdout (concatenate 'string
                                   "xrandr --output "
                                   "\"" primary-device "\""
                                   " --auto "
@@ -31,23 +16,52 @@
                                                    (concatenate 'string
                                                                 "--output "
                                                                 "\"" d "\""
+                                                                " --auto " (get-orientation position) " "
+                                                                "\"" primary-device "\" "))
+                                                 external-devices))
+                                  (apply #'concatenate 'string
+                                         (mapcar (lambda (d)
+                                                   (concatenate 'string
+                                                                "--output "
+                                                                "\"" d "\""
                                                                 " --off "))
-                                                 external-devices)))))
+                                                 disconnected-devices))))))
+
+(defun disconnect-external-displays (primary-device disconnected-devices)
+  "Connects all supplied connected displays."
+  (log-to-stdout (concatenate 'string
+                              "xrandr --output "
+                              "\"" primary-device "\""
+                              " --auto "
+                              (apply #'concatenate 'string
+                                     (mapcar (lambda (d)
+                                               (concatenate 'string
+                                                            "--output "
+                                                            "\"" d "\""
+                                                            " --off "))
+                                             disconnected-devices)))))
 
 (defun main ()
   "The main entry point to the program."
   (let* ((args (uiop:command-line-arguments)))
-    (if (not (member (first args) '("connect" "disconnect") :test #'string-equal))
-        (princ "Please specify \"connect/disconnect\"!")
-        (Let* ((connected-devices (concatenate 'list
-                                               (mapcar (lambda (x)
-                                                         (car (string-to-list x #\ )))
-                                                       (remove-if-not (lambda (x)
-                                                                        (string-equal (cadr (string-to-list x #\ )) "connected"))
-                                                                      (get-list-from-system "xrandr")))))
-               (primary-device (car connected-devices))
-               (external-devices (cdr connected-devices))
-               (orientation (second args)))
-          (if (string-equal (car args) "connect")
-              (connect-external-displays primary-device external-devices orientation)
-              (disconnect-external-displays primary-device external-devices))))))
+    (Let* ((connected-devices (concatenate 'list
+                                           (mapcar (lambda (x)
+                                                     (car (string-to-list x #\ )))
+                                                   (remove-if-not (lambda (x)
+                                                                    (string-equal (cadr (string-to-list x #\ )) "connected"))
+                                                                  (get-list-from-system "xrandr")))))
+           (disconnected-devices (concatenate 'list
+                                              (mapcar (lambda (x)
+                                                        (car (string-to-list x #\ )))
+                                                      (remove-if-not (lambda (x)
+                                                                       (string-equal (cadr (string-to-list x #\ )) "disconnected"))
+                                                                     (get-list-from-system "xrandr")))))
+           (primary-device (car connected-devices))
+           (external-devices (cdr connected-devices)))
+      (if (or (string-equal (first args) "disconnect") (null connected-devices))
+          (disconnect-external-displays primary-device
+                                        disconnected-devices)
+          (connect-external-displays primary-device
+                                     external-devices
+                                     disconnected-devices
+                                     (or (second args) "left"))))))
