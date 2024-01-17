ee2424cc66b5f14340d74d1eaf70ea74f15486f9
myTerminal <team.terminal@gmail.com>
2024-01-16 20:26:27 -0600

    simplify local variables

--- src/main.lisp
+++ src/main.lisp
@@ -29,31 +29,31 @@
 
 (defun main ()
   "The main entry point to the program."
-  (let* ((args (uiop:command-line-arguments)))
-    (Let* ((xrandr-devices (get-list-from-system "xrandr"))
-           (connected-devices (concatenate 'list
-                                           (mapcar (lambda (x)
-                                                     (car (string-to-list x #\ )))
-                                                   (remove-if-not (lambda (x)
-                                                                    (string-equal (cadr (string-to-list x #\ )) "connected"))
-                                                                  xrandr-devices))))
-           (disconnected-devices (concatenate 'list
-                                              (mapcar (lambda (x)
-                                                        (car (string-to-list x #\ )))
-                                                      (remove-if-not (lambda (x)
-                                                                       (string-equal (cadr (string-to-list x #\ )) "disconnected"))
-                                                                     xrandr-devices))))
-           (primary-device (car connected-devices))
-           (external-devices (cdr connected-devices))
-           (devices-to-connect (if (string-equal (first args) "disconnect")
-                                   nil
-                                   external-devices))
-           (devices-to-disconnect (if (string-equal (first args) "disconnect")
-                                      (concatenate 'list
-                                                   external-devices
-                                                   disconnected-devices)
-                                      disconnected-devices)))
-      (connect-or-disconnect primary-device
-                             devices-to-connect
-                             devices-to-disconnect
-                             (or (second args) "left")))))
+  (let* ((args (uiop:command-line-arguments))
+         (xrandr-devices (get-list-from-system "xrandr"))
+         (connected-devices (concatenate 'list
+                                         (mapcar (lambda (x)
+                                                   (car (string-to-list x #\ )))
+                                                 (remove-if-not (lambda (x)
+                                                                  (string-equal (cadr (string-to-list x #\ )) "connected"))
+                                                                xrandr-devices))))
+         (disconnected-devices (concatenate 'list
+                                            (mapcar (lambda (x)
+                                                      (car (string-to-list x #\ )))
+                                                    (remove-if-not (lambda (x)
+                                                                     (string-equal (cadr (string-to-list x #\ )) "disconnected"))
+                                                                   xrandr-devices))))
+         (primary-device (car connected-devices))
+         (external-devices (cdr connected-devices))
+         (devices-to-connect (if (string-equal (first args) "disconnect")
+                                 nil
+                                 external-devices))
+         (devices-to-disconnect (if (string-equal (first args) "disconnect")
+                                    (concatenate 'list
+                                                 external-devices
+                                                 disconnected-devices)
+                                    disconnected-devices)))
+    (connect-or-disconnect primary-device
+                           devices-to-connect
+                           devices-to-disconnect
+                           (or (second args) "left"))))
